5237d8e91dce1ad7590a36019fe7b5bb2b08ce7f
Tássio Rego <tassiorego@gmail.com>
2024-06-24 17:43:35 -0300

    chore: learn about variables in Go

---
+++ variables/variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "hello word"
+	fmt.Println(a)
+
+	var b, c int = 1, 3
+	fmt.Println(b, c)
+	fmt.Println(c + b)
+
+	var d = false
+	fmt.Println(d)
+
+	f := "short"
+	fmt.Println(f)
+}
